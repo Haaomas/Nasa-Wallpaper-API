9900de7af010d26439ba1934e72846bb51cf74ba
Haaomas <barreauji@gmail.com>
2022-08-16 15:22:36 +0200

    Part two Get the image
    - Get the image
    - Download it
    - Rename it

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import urllib.request, json
 """
 1 - Get the daily image from the NASA API (using the date of the computer)
 2 - Save the Image in a folder and rename it 
-3 - Rename the image with the date of the day 
-4 - Set a time when the wallpaper should change (10am) 
-5 - Change the wallpaper with the image of the day (same as the date of the computer)
-6 - Delete the image older than 7 days 
+3 - Set a time when the wallpaper should change (10am) 
+4 - Change the wallpaper with the image of the day (same as the date of the computer)
+5 - Delete the image older than 7 days 
 
 Bonus : 
 Keep the metadata of the img 
