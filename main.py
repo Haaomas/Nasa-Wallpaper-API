57b136b6565a8b7963eca3d7b9b753e02ee12121
Haaomas <barreauji@gmail.com>
2022-08-16 10:30:26 +0200

    Pseudocode of the project

---
+++ main.py
@@ -0,0 +1,14 @@
+    """
+    1 - Get the daily image from the NASA API (using the date of the computer)
+    2 - Save the Image in a folder 
+    3 - Rename the image with the date of the day 
+    4 - Set a time when the wallpaper should change (10am) 
+    5 - Change the wallpaper with the image of the day (same as the date of the computer)
+    6 - Delete the image older than 7 days 
+    
+    Bonus : 
+    Keep the metadata of the img 
+    """
+    
+    
+    
